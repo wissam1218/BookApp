9ad0dd2cd9c2c17da344786adf428da9d511627e
wissam1218 <wissam1218@gmail.com>
2020-03-15 16:32:11 -0400

    post test results save in text file

--- app/src/main/java/com/example/bookapp/resultActivity.java
+++ app/src/main/java/com/example/bookapp/resultActivity.java
@@ -2,11 +2,23 @@ package com.example.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.icu.util.Output;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 import static com.example.bookapp.postViewer.mScore;
 import static com.example.bookapp.postViewer.questionNum;
@@ -14,7 +26,7 @@ import static com.example.bookapp.postViewer.testNum;
 
 public class resultActivity extends AppCompatActivity {
     TextView mGrade, mFinalScore;
-    Button mRetryButton, nextBtn;
+    Button mRetryButton, nextBtn, saveBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +36,22 @@ public class resultActivity extends AppCompatActivity {
         mFinalScore = (TextView)findViewById(R.id.outOf);
         mRetryButton = (Button)findViewById(R.id.retry);
         nextBtn = (Button) findViewById(R.id.nextBtn);
+        saveBtn = (Button) findViewById(R.id.saveBtn);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save(resultActivity.this, "postReportCard.txt", mScore);
+
+            }
+        });
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 testNum++;
                 questionNum = 0;
+                mScore = 0;
                 Intent intent = new Intent(resultActivity.this, postViewer.class);
                 startActivity(intent);
             }
@@ -44,13 +66,19 @@ public class resultActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-        Bundle bundle = getIntent().getExtras();
-        int score = bundle.getInt("finalScore");
-
         mFinalScore.setText("You scored " + mScore);
-
-
+    }
+    public void save(Context context, String fileName, int score) {
+        try {
+            File root = new File(Environment.getExternalStorageDirectory(), "reports");
+            File file = new File(root, fileName);
+            FileWriter writer = new FileWriter(file);
+            writer.write("postTest number "+testNum+" result is " +score);
+            writer.flush();
+            writer.close();
+            Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
