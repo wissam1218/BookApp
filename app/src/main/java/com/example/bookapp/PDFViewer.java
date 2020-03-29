1b1456364106dd1c35744b94ec8b84b0e0164df4
wissam1218 <wissam1218@gmail.com>
2020-03-29 19:06:25 -0400

    pdf viewer works except for the first launch

--- app/src/main/java/com/example/bookapp/PDFViewer.java
+++ app/src/main/java/com/example/bookapp/PDFViewer.java
@@ -7,21 +7,16 @@ import com.github.barteksc.pdfviewer.util.FitPolicy;
 import com.nbsp.materialfilepicker.MaterialFilePicker;
 import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.TextView;
 import java.io.File;
 import java.util.regex.Pattern;
 
 public class PDFViewer extends AppCompatActivity {
     public static String filepath = "";
-    // the initial path will point too "math.pdf"
-    public static String path = "/storage/emulated/0/math.pdf";
-    TextView tvView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdfviewer);
-        tvView = findViewById(R.id.textView3);
         new MaterialFilePicker()
                 .withActivity(this)
                 .withRequestCode(1)
@@ -29,22 +24,7 @@ public class PDFViewer extends AppCompatActivity {
                 .withFilterDirectories(true) // Set directories filterable (false by default)
                 .withHiddenFiles(true) // Show hidden files and folders
                 .start();
-        loadPDF();
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-           filepath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
-            tvView.setText(filepath); // this path is shown to be correct
-            path = filepath;
-        }
-    }
-    // function to load specified pdf
-    private void loadPDF(){
-        File file = new File(path);
+        File file = new File(filepath);
         PDFView pdf = findViewById(R.id.pdfView);
         pdf.fromFile(file).enableSwipe(true)
                 .swipeHorizontal(false)
@@ -61,5 +41,12 @@ public class PDFViewer extends AppCompatActivity {
                 .nightMode(true)
                 .load();
     }
-
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            filepath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+        }
+    }
 }
